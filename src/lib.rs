c9abc26fd113b048777e352deb243e757c23337e
Marvin Vogt <m@rvinvogt.com>
2023-07-19 21:38:20 +0000

    Add attribute name and single value parsers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod parser;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
---
+++ src/parser.rs
@@ -0,0 +1,45 @@
+use nom::{
+    bytes::complete::{tag, take_until, take_while},
+    character::complete::space0,
+    IResult,
+};
+
+fn attribute_name(input: &str) -> IResult<&str, &str> {
+    let (remaining, name) = take_while(|c: char| c.is_alphanumeric() || c == '-')(input)?;
+    let (remaining, _) = tag(":")(remaining)?;
+
+    Ok((remaining, name))
+}
+
+fn attribute_value(input: &str) -> IResult<&str, &str> {
+    let (remaining, _) = space0(input)?;
+    let (remaining, value) = take_until("\n")(remaining)?;
+    let (remaining, _) = tag("\n")(remaining)?;
+
+    Ok((remaining, value))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{attribute_name, attribute_value};
+
+    #[test]
+    fn parse_attribute_name() {
+        let input = "example-name:        example-value\n";
+
+        let (rest, result) = attribute_name(input).unwrap();
+
+        assert_eq!(rest, "        example-value\n");
+        assert_eq!(result, "example-name");
+    }
+
+    #[test]
+    fn parse_single_value() {
+        let input = "        example-value\n";
+
+        let (rest, result) = attribute_value(input).unwrap();
+
+        assert_eq!(rest, "");
+        assert_eq!(result, "example-value");
+    }
+}
