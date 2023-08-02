fe9cacbdd98e0807521b5fc851b1e90983535ea0
Marvin Vogt <m@rvinvogt.com>
2023-08-02 20:32:11 +0000

    Expose `parse_rpsl_server_response`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@
 #![forbid(unsafe_code)]
 #![doc = include_str!("../README.md")]
 
-pub use parser::parse_rpsl_object;
+pub use parser::{parse_rpsl_object, parse_rpsl_server_response};
 mod parser;
 mod rpsl;
