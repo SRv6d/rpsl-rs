928f0534166591f21c9f8a0de7babfbc1b994f9f
Marvin Vogt <m@rvinvogt.com>
2023-08-04 13:07:31 +0000

    Move pyo3 to module

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,5 @@
 
 pub use parser::{parse_rpsl_object, parse_rpsl_server_response};
 mod parser;
+mod pyo3;
 mod rpsl;
-
-use pyo3::prelude::*;
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
-#[pymodule]
-fn rpsl_parser(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
