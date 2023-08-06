058cf3c68f56d71c1fe30b2a2a0acd31684dc324
Marvin Vogt <m@rvinvogt.com>
2023-08-06 15:33:33 +0000

    Add test types with respective hypothesis strategies

---
+++ tests/python/rpsl_test_types.py
@@ -0,0 +1,160 @@
+"""Types representing RPSL components for testing purposes."""
+
+from typing import NamedTuple
+
+
+class TestRpslAttributeSingleValue(NamedTuple):
+    """A RPSL attribute that has a single value.
+
+    Represents a RPSL attribute containing a single value for testing purposes.
+
+    Attributes:
+        text: The RPSL text representation of the attribute.
+        name: The name of the attribute.
+        value: The value of the attribute.
+
+    Example:
+        ```
+        aut-num:        AS51531
+        ^^^^^^^ - name  ^^^^^^^ - value
+        ^^^^^^^^^^^^^^^^^^^^^^^ - text
+    """
+
+    text: str
+    name: str
+    value: str
+
+
+class TestRpslAttributeMultiValue(NamedTuple):
+    """A RPSL attribute that has multiple values.
+
+    Represents a RPSL attribute containing multiple values for testing purposes.
+
+    Attributes:
+        text: The RPSL text representation of the attribute.
+        name: The name of the attribute.
+        value: The value of the attribute.
+
+    Example:
+        ```
+        remarks:        The first value
+                        ^^^^^^^^^^^^^^^ - value[0]
+                        The second value
+                        ^^^^^^^^^^^^^^^^ - value[1]
+        ```
+    """
+
+    text: str
+    name: str
+    value: tuple[str | None, ...]
+
+
+class TestRpslAttributeNoneValue(NamedTuple):
+    """A RPSL attribute that has no value.
+
+    Represents a RPSL attribute containing no value for testing purposes.
+
+    Attributes:
+        text: The RPSL text representation of the attribute.
+        name: The name of the attribute.
+        value: The value of the attribute.
+
+    Example:
+        ```
+        remarks:                  # intentional trailing whitespace
+                        ^^^^^^^^^^ - whitespace where we would expect a value
+        ```
+    """
+
+    text: str
+    name: str
+    value: None
+
+
+class TestRpslTextObject(
+    tuple[
+        TestRpslAttributeSingleValue
+        | TestRpslAttributeMultiValue
+        | TestRpslAttributeNoneValue,
+        ...,
+    ]
+):
+    """A RPSL text object.
+
+    Represents a RPSL text object for testing purposes.
+    RPSL text consists of one or more (multiline) attributes.
+
+    Example:
+        ```
+        aut-num:        AS51531                               <- [0]
+        remarks:                                              <- [1]
+        remarks:        For peering information please check: <- [2]
+                        https://as51531.peeringdb.com         <- [2]
+        mnt-by:         DECIX-MNT                             <- [3]
+        ```
+    """
+
+    @property
+    def text(self) -> str:
+        """The RPSL text representation of the object."""
+        repr_ = "\n".join(attribute.text for attribute in self)
+        repr_ += "\n" * 2  # Objects end with a blank line
+
+        return repr_
+
+
+class TestWhoisServerMessage(NamedTuple):
+    """A whois server message.
+
+    Example:
+        ```
+        % Information related to 'AS51531'
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - value
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - text
+        ```
+    """
+
+    text: str
+    value: str
+
+
+class TestRpslWhoisServerResponse(
+    tuple[TestRpslTextObject | TestWhoisServerMessage, ...]
+):
+    """A whois server response.
+
+    Represents a text response from a whois server possibly containing
+    multiple RPSL objects as well as server messages.
+
+
+    Example:
+        ```
+        % Information related to 'AS51531'               <- server message
+
+        % Abuse contact for 'AS51531' is 'abuse@cix.net' <- server message
+
+        aut-num:        AS51531                          <- start of first RPSL object
+        remarks:
+        remarks:        For peering information please check:
+                        https://as51531.peeringdb.com
+        mnt-by:         DECIX-MNT
+        descr:          DE-CIX Management GmbH
+        descr:          DE
+        admin-c:        ENG6695-RIPE
+        tech-c:         ENG6695-RIPE
+        status:         ASSIGNED
+        org:            ORG-DtGI1-RIPE
+        mnt-by:         RIPE-NCC-END-MNT
+        created:        2010-09-21T11:26:20Z
+        last-modified:  2019-12-04T11:19:36Z
+        source:         RIPE
+        ```
+    """
+
+    @property
+    def text(self) -> str:
+        """The text representation of the server response."""
+        repr_ = "\n".join(_.text for _ in self)
+        repr_ += "\n"
+
+        return repr_
