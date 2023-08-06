07ebe362b3577f9877127cb3596d4c57bac8baa7
Marvin Vogt <m@rvinvogt.com>
2023-08-06 20:16:44 +0000

    Add property based test of RPSL parsing

---
+++ tests/property_based/test_parsing.py
@@ -0,0 +1,31 @@
+from typing import TypeAlias
+
+import pytest
+from hypothesis import given
+from rpsl_parser import parse_rpsl_object
+
+from property_based import strategies
+from property_based.rpsl_test_types import (
+    TestRpslTextObject,
+    TestRpslAttributeMultiValue,
+)
+
+_RpslAttributes: TypeAlias = list[str]
+
+
+@given(strategies.rpsl_text_object())
+def test_property_based_rpsl_object(rpsl: TestRpslTextObject):
+    """A property based RPSL object is parsed correctly."""
+    expected = tuple(
+        (
+            attr.name,
+            (attr.value,)
+            if not isinstance(attr, TestRpslAttributeMultiValue)
+            else attr.value,
+        )
+        for attr in rpsl
+    )
+
+    parsed = parse_rpsl_object(rpsl.text)
+
+    assert parsed == expected
