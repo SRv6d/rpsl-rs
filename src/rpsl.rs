1e3e068f65c8313435273f91136b6f86f710e8a7
Marvin Vogt <m@rvinvogt.com>
2023-08-07 19:49:21 +0000

    Make rpsl module public and add required documentation

--- src/rpsl.rs
+++ src/rpsl.rs
@@ -1,19 +1,26 @@
+//! Types for representing RPSL components.
 use std::option::Option;
 
+/// Represents a RPSL attribute.
 #[derive(Debug, PartialEq, Eq)]
 pub struct Attribute {
+    /// The name of the attribute.
     pub name: String,
+    /// The values of the attribute.
+    /// Single line attributes only have one value, while for multi-line attributes every line is a value.
+    /// An empty value or one containing only whitespace is represented as None.
     pub values: Vec<Option<String>>,
 }
 
 impl Attribute {
+    /// Create a new RPSL attribute from a name and vector of values.
     pub fn new(name: String, values: Vec<Option<String>>) -> Self {
         Attribute { name, values }
     }
 }
 
-// Create an RPSL attribute from a tuple of slices parsed from RPSL text.
-// An empty value or one containing only whitespace is converted to None.
+/// Create a RPSL attribute from a tuple of slices parsed from RPSL text.
+/// An empty value or one containing only whitespace is converted to None.
 impl From<(&str, Vec<&str>)> for Attribute {
     fn from(attribute_slice: (&str, Vec<&str>)) -> Self {
         let (name, values) = attribute_slice;
@@ -33,10 +40,12 @@ impl From<(&str, Vec<&str>)> for Attribute {
     }
 }
 
+/// Represents a RPSL object.
 #[derive(Debug, PartialEq, Eq)]
 pub struct Object(Vec<Attribute>);
 
 impl Object {
+    /// Create a new RPSL object from a vector of attributes.
     pub fn new(attributes: Vec<Attribute>) -> Self {
         Object(attributes)
     }
@@ -63,11 +72,12 @@ impl From<Vec<(&str, Vec<&str>)>> for Object {
         Object(attributes)
     }
 }
-
+///Represents a collection of RPSL objects, for example as the result of a whois query.
 #[derive(Debug, PartialEq, Eq)]
 pub struct ObjectCollection(Vec<Object>);
 
 impl ObjectCollection {
+    /// Create a new RPSL object collection from a vector of objects.
     pub fn new(objects: Vec<Object>) -> Self {
         ObjectCollection(objects)
     }
