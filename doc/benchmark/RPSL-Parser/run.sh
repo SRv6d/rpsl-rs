d635ad500fe3ed27cf9c9fb5061f11ff29293a29
Marvin Vogt <m@rvinvogt.com>
2023-08-11 11:52:32 +0000

    Install rustup and hyperfine in `./run.sh`

--- doc/benchmark/RPSL-Parser/run.sh
+++ doc/benchmark/RPSL-Parser/run.sh
@@ -1,3 +1,10 @@
+export NEEDRESTART_MODE=a
+export DEBIAN_FRONTEND=noninteractive
+sudo apt update && sudo apt install -y build-essential
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+cargo install --locked hyperfine
+
 sudo PERL_MM_USE_DEFAULT=1 cpan App::cpanminus
 sudo cpanm RPSL::Parser
 
