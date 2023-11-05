69eda752053fd62fc6552768571b82be5dcf1ac2
Marvin Vogt <m@rvinvogt.com>
2023-11-05 23:27:57 +0000

    Add examples to `Object`

--- src/rpsl/object.rs
+++ src/rpsl/object.rs
@@ -1,6 +1,39 @@
 use crate::rpsl::Attribute;
 
 /// An RPSL object.
+///
+/// # Examples
+///
+/// A role object for the ACME corporation.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// let role_acme = Object::new(vec![
+///     Attribute::new("role", "ACME Company"),
+///     Attribute::new("address", "Packet Street 6"),
+///     Attribute::new("address", "128 Series of Tubes"),
+///     Attribute::new("address", "Internet"),
+///     Attribute::new("email", "rpsl-parser@github.com"),
+///     Attribute::new("nic-hdl", "RPSL1-RIPE"),
+///     Attribute::new("source", "RIPE"),
+/// ]);
+/// ```
+///
+/// Each attribute can be accessed by index.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// # let role_acme = Object::new(vec![
+/// #     Attribute::new("role", "ACME Company"),
+/// #     Attribute::new("address", "Packet Street 6"),
+/// #     Attribute::new("address", "128 Series of Tubes"),
+/// #     Attribute::new("address", "Internet"),
+/// #     Attribute::new("email", "rpsl-parser@github.com"),
+/// #     Attribute::new("nic-hdl", "RPSL1-RIPE"),
+/// #     Attribute::new("source", "RIPE"),
+/// # ]);
+/// assert_eq!(role_acme[0], Attribute::new("role", "ACME Company"));
+/// assert_eq!(role_acme[6], Attribute::new("source", "RIPE"));
+/// ```
+///
 #[derive(Debug, PartialEq, Eq)]
 pub struct Object(Vec<Attribute>);
 
