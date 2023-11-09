f75657a9f9665e0c93d55af58d6b0217391a567d
Marvin Vogt <m@rvinvogt.com>
2023-11-09 21:21:45 +0000

    Disallow use of `panic!` using clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::pedantic)]
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
+#![forbid(clippy::panic)]
 #![doc = include_str!("../README.md")]
 
 pub use parser::{parse_object, parse_whois_response};
