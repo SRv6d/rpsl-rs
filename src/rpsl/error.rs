88d2448379770f106d1a238004f35e27384e3050
Marvin Vogt <m@rvinvogt.com>
2023-11-09 23:30:40 +0000

    Add `InvalidValueError`

--- src/rpsl/error.rs
+++ src/rpsl/error.rs
@@ -7,15 +7,15 @@ pub enum InvalidNameError {
 }
 
 #[derive(Error, Debug)]
+pub enum InvalidValueError {}
+
+#[derive(Error, Debug)]
 /// An error that can occur when parsing or trying to create an attribute that is invalid.
 pub enum AttributeError {
     /// The name of the attribute is invalid.
     #[error("Invalid attribute name: {0}")]
-    InvalidName(InvalidNameError),
-}
-
-impl From<InvalidNameError> for AttributeError {
-    fn from(error: InvalidNameError) -> Self {
-        AttributeError::InvalidName(error)
-    }
+    InvalidName(#[from] InvalidNameError),
+    /// The value of the attribute is invalid.
+    #[error("Invalid attribute value: {0}")]
+    InvalidValue(#[from] InvalidValueError),
 }
