6d07742d12036369795301c238c08d8987e65fa7
Marvin Vogt <m@rvinvogt.com>
2023-11-26 16:03:48 +0000

    Allow module name repetition

--- src/rpsl/borrowed.rs
+++ src/rpsl/borrowed.rs
@@ -1,5 +1,7 @@
 pub use self::attribute::AttributeView;
 pub use self::object::ObjectView;
 
+#[allow(clippy::module_name_repetitions)]
 mod attribute;
+#[allow(clippy::module_name_repetitions)]
 mod object;
