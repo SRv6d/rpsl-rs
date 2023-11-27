d2acba282dee54ccee3651ebc1e6cbd46d7703c0
Marvin Vogt <m@rvinvogt.com>
2023-11-27 23:17:08 +0000

    Simplify `ObjectView` by using newtype

--- src/rpsl/borrowed/object.rs
+++ src/rpsl/borrowed/object.rs
@@ -60,12 +60,10 @@ use super::attribute::AttributeView;
 /// # }
 /// ```
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct ObjectView<'a> {
-    pub attributes: Vec<AttributeView<'a>>,
-}
+pub struct ObjectView<'a>(Vec<AttributeView<'a>>);
 
 impl<'a> ObjectView<'a> {
     pub(crate) fn new(attributes: Vec<AttributeView<'a>>) -> Self {
-        Self { attributes }
+        Self(attributes)
     }
 }
