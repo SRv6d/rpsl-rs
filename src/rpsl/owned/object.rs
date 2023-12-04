96061d5363b2e3e615cf01c8c53677bb1db6a9a4
Marvin Vogt <m@rvinvogt.com>
2023-12-04 21:46:35 +0100

    Re add adjusted docstring

--- src/rpsl/owned/object.rs
+++ src/rpsl/owned/object.rs
@@ -2,6 +2,110 @@ use super::Attribute;
 use std::ops::Index;
 
 /// An RPSL object.
+///
+/// ```text
+/// ┌───────────────────────────────────────────────┐
+/// │  Object                                       │
+/// ├───────────────────────────────────────────────┤
+/// │  [role]    ───  ACME Company                  │
+/// │  [address] ──┬─ Packet Street 6               │
+/// │              ├─ 128 Series of Tubes           │
+/// │              └─ Internet                      │
+/// │  [email]   ───  rpsl-parser@github.com        │
+/// │  [nic-hdl] ───  RPSL1-RIPE                    │
+/// │  [source]  ───  RIPE                          │
+/// └───────────────────────────────────────────────┘
+/// ```
+///
+/// # Examples
+///
+/// A role object for the ACME corporation.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let role_acme = Object::new(vec![
+///     Attribute::new("role".parse()?, "ACME Company".parse()?),
+///     Attribute::new("address".parse()?, "Packet Street 6".parse()?),
+///     Attribute::new("address".parse()?, "128 Series of Tubes".parse()?),
+///     Attribute::new("address".parse()?, "Internet".parse()?),
+///     Attribute::new("email".parse()?, "rpsl-parser@github.com".parse()?),
+///     Attribute::new("nic-hdl".parse()?, "RPSL1-RIPE".parse()?),
+///     Attribute::new("source".parse()?, "RIPE".parse()?),
+/// ]);
+/// # Ok(())
+/// # }
+/// ```
+///
+/// /// Each attribute can be accessed by index.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let role_acme = Object::new(vec![
+/// #     Attribute::new("role".parse()?, "ACME Company".parse()?),
+/// #     Attribute::new("address".parse()?, "Packet Street 6".parse()?),
+/// #     Attribute::new("address".parse()?, "128 Series of Tubes".parse()?),
+/// #     Attribute::new("address".parse()?, "Internet".parse()?),
+/// #     Attribute::new("email".parse()?, "rpsl-parser@github.com".parse()?),
+/// #     Attribute::new("nic-hdl".parse()?, "RPSL1-RIPE".parse()?),
+/// #     Attribute::new("source".parse()?, "RIPE".parse()?),
+/// # ]);
+/// assert_eq!(role_acme[0], Attribute::new("role".parse()?, "ACME Company".parse()?));
+/// assert_eq!(role_acme[6], Attribute::new("source".parse()?, "RIPE".parse()?));
+/// # Ok(())
+/// # }
+/// ```
+///
+/// While specific attribute values can be accessed by name.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let role_acme = Object::new(vec![
+/// #     Attribute::new("role".parse()?, "ACME Company".parse()?),
+/// #     Attribute::new("address".parse()?, "Packet Street 6".parse()?),
+/// #     Attribute::new("address".parse()?, "128 Series of Tubes".parse()?),
+/// #     Attribute::new("address".parse()?, "Internet".parse()?),
+/// #     Attribute::new("email".parse()?, "rpsl-parser@github.com".parse()?),
+/// #     Attribute::new("nic-hdl".parse()?, "RPSL1-RIPE".parse()?),
+/// #     Attribute::new("source".parse()?, "RIPE".parse()?),
+/// # ]);
+/// assert_eq!(role_acme.get("role"), vec!["ACME Company"]);
+/// assert_eq!(role_acme.get("address"), vec!["Packet Street 6", "128 Series of Tubes", "Internet"]);
+/// assert_eq!(role_acme.get("email"), vec!["rpsl-parser@github.com"]);
+/// assert_eq!(role_acme.get("nic-hdl"), vec!["RPSL1-RIPE"]);
+/// assert_eq!(role_acme.get("source"), vec!["RIPE"]);
+/// # Ok(())
+/// # }
+/// ```
+///
+/// The entire object can also be represented as RPSL.
+/// ```
+/// # use rpsl_parser::{Attribute, Object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let role_acme = Object::new(vec![
+/// #     Attribute::new("role".parse()?, "ACME Company".parse()?),
+/// #     Attribute::new("address".parse()?, "Packet Street 6".parse()?),
+/// #     Attribute::new("address".parse()?, "128 Series of Tubes".parse()?),
+/// #     Attribute::new("address".parse()?, "Internet".parse()?),
+/// #     Attribute::new("email".parse()?, "rpsl-parser@github.com".parse()?),
+/// #     Attribute::new("nic-hdl".parse()?, "RPSL1-RIPE".parse()?),
+/// #     Attribute::new("source".parse()?, "RIPE".parse()?),
+/// # ]);
+/// assert_eq!(
+///    role_acme.to_string(),
+///    concat!(
+///        "role:           ACME Company\n",
+///        "address:        Packet Street 6\n",
+///        "address:        128 Series of Tubes\n",
+///        "address:        Internet\n",
+///        "email:          rpsl-parser@github.com\n",
+///        "nic-hdl:        RPSL1-RIPE\n",
+///        "source:         RIPE\n",
+///        "\n"
+///    )
+/// );
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Object(Vec<Attribute>);
 
