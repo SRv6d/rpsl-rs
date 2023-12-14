a16222fd4b359399081460c973084c38fe64b6f7
Marvin Vogt <m@rvinvogt.com>
2023-12-14 22:35:41 +0000

    Move error module to top of crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 #![forbid(clippy::panic)]
 #![doc = include_str!("../README.md")]
 
+pub use error::AttributeError;
 pub use parser::{parse_object, parse_whois_response};
-pub use rpsl::error::AttributeError;
 pub use rpsl::{Attribute, AttributeView, Object, ObjectView};
 
+#[allow(clippy::module_name_repetitions)]
+mod error;
 mod parser;
 mod rpsl;
--- src/rpsl.rs
+++ src/rpsl.rs
@@ -3,6 +3,4 @@ pub use self::owned::{Attribute, Name, Object, Value};
 
 mod borrowed;
 mod common;
-#[allow(clippy::module_name_repetitions)]
-pub mod error;
 mod owned;
