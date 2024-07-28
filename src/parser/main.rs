2c9e9869bf0282eed417d829a284fc22e5799ea6
Marvin Vogt <m@rvinvogt.com>
2024-07-28 19:51:15 +0000

    Implement `parse_whois_response`

--- src/parser/main.rs
+++ src/parser/main.rs
@@ -2,7 +2,7 @@ use super::component;
 use crate::{Attribute, Object};
 use winnow::{
     ascii::{multispace0, newline},
-    combinator::{delimited, repeat, terminated, todo},
+    combinator::{alt, delimited, repeat, terminated},
     PResult, Parser,
 };
 
@@ -16,14 +16,19 @@ fn object_block<'s>(input: &mut &'s str) -> PResult<Object<'s>> {
     Ok(Object::from_parsed(input, attributes))
 }
 
-/// Uses the object block parser but allows for optional padding with server messages or newlines.
-fn padded_object_block<'s>(input: &mut &'s str) -> PResult<Object<'s>> {
-    todo.parse_next(input)
+/// Extends the object block parser to consume optional padding server messages or newlines.
+fn object_block_padded<'s>(input: &mut &'s str) -> PResult<Object<'s>> {
+    delimited(
+        consume_opt_message_or_newlines,
+        object_block,
+        consume_opt_message_or_newlines,
+    )
+    .parse_next(input)
 }
 
-/// Consume an unlimited number of optional server messages or newlines.
+/// Consume optional server messages or newlines.
 fn consume_opt_message_or_newlines(input: &mut &str) -> PResult<()> {
-    todo!()
+    repeat(0.., alt((newline.void(), component::server_message.void()))).parse_next(input)
 }
 
 /// Parse RPSL into an [`Object`], borrowing from the source.
@@ -212,7 +217,7 @@ pub fn parse_object(rpsl: &str) -> Result<Object, ()> {
 /// # Ok(())
 /// # }
 pub fn parse_whois_response(response: &str) -> Result<Vec<Object>, ()> {
-    let objects = repeat(1.., padded_object_block).parse(response).unwrap();
+    let objects = repeat(1.., object_block_padded).parse(response).unwrap();
     Ok(objects)
 }
 
