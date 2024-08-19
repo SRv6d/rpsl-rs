a408da9f65822939b207cc7d831879ea57a97d80
Marvin Vogt <m@rvinvogt.com>
2024-08-19 22:02:56 +0000

    Remove owned and borrowed modules

--- src/borrowed/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use self::attribute::AttributeView;
-pub use self::object::ObjectView;
-
-#[allow(clippy::module_name_repetitions)]
-mod attribute;
-#[allow(clippy::module_name_repetitions)]
-mod object;
