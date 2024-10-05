f393fcae999115b5bc8ec0a88a9bdcca7681c4c8
Marvin Vogt <m@rvinvogt.com>
2024-10-05 12:12:41 +0000

    Add test for Object deref impl

--- src/object.rs
+++ src/object.rs
@@ -499,6 +499,23 @@ mod tests {
 
     #[rstest]
     #[case(
+        vec![
+            Attribute::unchecked_single("role", "ACME Company"),
+            Attribute::unchecked_single("address", "Packet Street 6"),
+            Attribute::unchecked_single("address", "128 Series of Tubes"),
+            Attribute::unchecked_single("address", "Internet"),
+            Attribute::unchecked_single("email", "rpsl-rs@github.com"),
+            Attribute::unchecked_single("nic-hdl", "RPSL1-RIPE"),
+            Attribute::unchecked_single("source", "RIPE"),
+        ],
+    )]
+    fn object_deref(#[case] attributes: Vec<Attribute<'static>>) {
+        let object = Object::new(attributes.clone());
+        assert_eq!(*object, attributes);
+    }
+
+    #[rstest]
+    #[case(
         Object::new(vec![
             Attribute::unchecked_single("role", "ACME Company"),
             Attribute::unchecked_single("address", "Packet Street 6"),
