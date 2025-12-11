df1475de91eb460932287acaf8d0f16a5ee8ff85
Marvin Vogt <m@rvinvogt.com>
2025-12-11 22:22:01 +0000

    Use single file parser module

--- src/parser.rs
+++ src/parser.rs
@@ -9,11 +9,200 @@ use winnow::{
 
 use crate::{Attribute, Name, Object, Value};
 
+/// Parse RPSL into an [`Object`], borrowing from the source.
+///
+/// ```text
+/// role:           ACME Company
+/// address:        Packet Street 6
+/// address:        128 Series of Tubes
+/// address:        Internet
+/// email:          rpsl-rs@github.com
+/// nic-hdl:        RPSL1-RIPE
+/// source:         RIPE
+///                        ↓
+/// role:           ACME Company ◀─────────────── &"role":      &"ACME Company"
+/// address:        Packet Street 6 ◀──────────── &"address":   &"Packet Street 6"
+/// address:        128 Series of Tubes ◀──────── &"address":   &"128 Series of Tubes"
+/// address:        Internet ◀─────────────────── &"address":   &"Internet"
+/// email:          rpsl-rs@github.com ◀───────── &"email":     &"rpsl-rs@github.com"
+/// nic-hdl:        RPSL1-RIPE ◀───────────────── &"nic-hdl":   &"RPSL1-RIPE"
+/// source:         RIPE ◀─────────────────────── &"source":    &"RIPE"
+/// ```
+///
+/// # Errors
+/// Returns a [`ParseError`] if the input is not valid RPSL.
+///
+/// # Examples
+/// ```
+/// # use rpsl::{parse_object, object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let role_acme = "
+/// role:        ACME Company
+/// address:     Packet Street 6
+/// address:     128 Series of Tubes
+/// address:     Internet
+/// email:       rpsl-rs@github.com
+/// nic-hdl:     RPSL1-RIPE
+/// source:      RIPE
+///
+/// ";
+/// let parsed = parse_object(role_acme)?;
+/// assert_eq!(
+///     parsed,
+///     object! {
+///         "role": "ACME Company";
+///         "address": "Packet Street 6";
+///         "address": "128 Series of Tubes";
+///         "address": "Internet";
+///         "email": "rpsl-rs@github.com";
+///         "nic-hdl": "RPSL1-RIPE";
+///         "source": "RIPE";
+///     }
+/// );
+/// # Ok(())
+/// # }
+/// ```
+///
+/// Values spread over multiple lines can be parsed too.
+/// ```
+/// # use rpsl::{parse_object, object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let multiline_remark = "
+/// remarks:     Value 1
+///              Value 2
+///
+/// ";
+/// assert_eq!(
+///     parse_object(multiline_remark)?,
+///     object! {
+///         "remarks": "Value 1", "Value 2";
+///     }
+/// );
+/// # Ok(())
+/// # }
+/// ```
+///
+/// An attribute that does not have a value is valid.
+/// ```
+/// # use rpsl::{parse_object, object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let without_value = "
+/// as-name:     REMARKABLE
+/// remarks:
+/// remarks:     ^^^^^^^^^^ nothing here
+///
+/// ";
+/// assert_eq!(
+///     parse_object(without_value)?,
+///     object! {
+///         "as-name": "REMARKABLE";
+///         "remarks": "";
+///         "remarks": "^^^^^^^^^^ nothing here";
+///     }
+/// );
+/// # Ok(())
+/// # }
+/// ```
+///
+/// The same goes for values containing only whitespace.
+/// Since whitespace to the left of a value is trimmed, they are equivalent to no value.
+///
+/// ```
+/// # use rpsl::{parse_object, object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let whitespace_value = "
+/// as-name:     REMARKABLE
+/// remarks:               
+/// remarks:     ^^^^^^^^^^ nothing but hot air (whitespace)
+///
+/// ";
+/// assert_eq!(
+///     parse_object(whitespace_value)?,
+///     object! {
+///         "as-name": "REMARKABLE";
+///         "remarks": "";
+///         "remarks": "^^^^^^^^^^ nothing but hot air (whitespace)";
+///     }
+/// );
+/// # Ok(())
+/// # }
+/// ```
+pub fn parse_object(rpsl: &str) -> Result<Object<'_>, ParseError> {
+    let o = object_block().parse(rpsl)?;
+    Ok(o)
+}
+
+/// Parse a WHOIS server response into [`Object`]s contained within.
+///
+/// # Errors
+/// Returns a [`ParseError`] error if the input is not valid RPSL.
+///
+/// # Examples
+/// ```
+/// # use rpsl::{parse_whois_response, object};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let whois_response = "
+/// ASNumber:       32934
+/// ASName:         FACEBOOK
+/// ASHandle:       AS32934
+/// RegDate:        2004-08-24
+/// Updated:        2012-02-24
+/// Comment:        Please send abuse reports to abuse@facebook.com
+/// Ref:            https://rdap.arin.net/registry/autnum/32934
+///
+///
+/// OrgName:        Facebook, Inc.
+/// OrgId:          THEFA-3
+/// Address:        1601 Willow Rd.
+/// City:           Menlo Park
+/// StateProv:      CA
+/// PostalCode:     94025
+/// Country:        US
+/// RegDate:        2004-08-11
+/// Updated:        2012-04-17
+/// Ref:            https://rdap.arin.net/registry/entity/THEFA-3
+///
+/// ";
+/// let objects = parse_whois_response(whois_response)?;
+/// assert_eq!(
+///     objects,
+///     vec![
+///             object! {
+///                 "ASNumber": "32934";
+///                 "ASName": "FACEBOOK";
+///                 "ASHandle": "AS32934";
+///                 "RegDate": "2004-08-24";
+///                 "Updated": "2012-02-24";
+///                 "Comment": "Please send abuse reports to abuse@facebook.com";
+///                 "Ref": "https://rdap.arin.net/registry/autnum/32934";
+///             },
+///             object! {
+///                 "OrgName": "Facebook, Inc.";
+///                 "OrgId": "THEFA-3";
+///                 "Address": "1601 Willow Rd.";
+///                 "City": "Menlo Park";
+///                 "StateProv": "CA";
+///                 "PostalCode": "94025";
+///                 "Country": "US";
+///                 "RegDate": "2004-08-11";
+///                 "Updated": "2012-04-17";
+///                 "Ref": "https://rdap.arin.net/registry/entity/THEFA-3";
+///             }
+///         ]
+/// );
+/// # Ok(())
+/// # }
+pub fn parse_whois_response(response: &str) -> Result<Vec<Object<'_>>, ParseError> {
+    let block_parser = object_block_padded(object_block());
+    let objects = repeat(1.., block_parser).parse(response)?;
+    Ok(objects)
+}
+
 /// Parse a list of attributes that make up an object.
 ///
 /// Consumes optional surrounding whitespace, then reads attributes
 /// until the mandatory blank line that terminates the object.
-pub fn object_block<'s>() -> impl Parser<&'s str, Object<'s>, ErrMode<ContextError>> {
+fn object_block<'s>() -> impl Parser<&'s str, Object<'s>, ErrMode<ContextError>> {
     // a list of attributes that ends when a blank line is encountered, as per RFC 2622.
     let object = terminated(repeat(1.., attribute()), newline);
 
@@ -25,7 +214,7 @@ pub fn object_block<'s>() -> impl Parser<&'s str, Object<'s>, ErrMode<ContextErr
 
 /// Generate a parser that extends the given object block parser to consume optional padding
 /// server messages or newlines.
-pub fn object_block_padded<'s, P>(
+fn object_block_padded<'s, P>(
     block_parser: P,
 ) -> impl Parser<&'s str, Object<'s>, ErrMode<ContextError>>
 where
