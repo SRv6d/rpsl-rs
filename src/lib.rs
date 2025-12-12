dd470a2b064b43305dfd6f47707beda549dfb60e
Marvin Vogt <m@rvinvogt.com>
2025-12-12 23:32:32 +0000

    Remove display example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! A Routing Policy Specification Language (RPSL) parser with a focus on speed and correctness.
-//! 
+//!
 //! ⚡️ 130-250x faster than other parsers\
 //! 📰 Complete implementation for multiline RPSL values\
 //! 💬 Able to parse objects directly from whois server responses\
@@ -7,14 +7,14 @@
 //! 🧪 Robust parsing of any valid input ensured by Property Based Tests
 //!
 //! ## Usage
-//! 
+//!
 //! ### Parsing RPSL objects
-//! 
-//! A string containing an object in RPSL notation can be parsed to an [Object] using the [`parse_object`] function.
-//! 
+//!
+//! A string containing an object in RPSL notation can be parsed to an [`Object`] using the [`parse_object`] function.
+//!
 //! ```rust
 //! use rpsl::parse_object;
-//! 
+//!
 //! let role_acme = "
 //! role:        ACME Company
 //! address:     Packet Street 6
@@ -23,14 +23,15 @@
 //! email:       rpsl-rs@github.com
 //! nic-hdl:     RPSL1-RIPE
 //! source:      RIPE
-//! 
+//!
 //! ";
-//! let parsed = parse_object(role_acme).unwrap();
+//! let parsed = parse_object(role_acme)?;
+//! # Ok::<(), Box<dyn std::error::Error>>(())
 //! ```
-//! 
-//! This returns an [Object], a sorted collection of [Attributes] that contain string references
+//!
+//! This returns an [`Object`], a sorted [`Attribute`] collection which contain string references
 //! pointing to attributes and their values from the parsed text.
-//! 
+//!
 //! ```text
 //! role:           ACME Company ◀─────────────── &"role":      &"ACME Company"
 //! address:        Packet Street 6 ◀──────────── &"address":   &"Packet Street 6"
@@ -40,27 +41,12 @@
 //! nic-hdl:        RPSL1-RIPE ◀───────────────── &"nic-hdl":   &"RPSL1-RIPE"
 //! source:         RIPE ◀─────────────────────── &"source":    &"RIPE"
 //! ```
-//! 
-//! This is what makes `rpsl-rs` performant and memory efficient, since no additional allocation is required during parsing.
-//! 
-//! Each [Attribute] can be accessed by its index and has a name and value.
-//! 
-//! ```rust,ignore
-//! println!("{:#?}", parsed[1]);
-//! 
-//! Attribute {
-//!     name: Name("address"),
-//!     value: SingleLine(Some("Packet Street 6")),
-//! }
-//! ```
-//! 
-//! Since RPSL attribute values can either be single- or multiline, two different variants are used to represent them. See [Attribute] and [`parse_object`] for more details and examples.
-//! 
+//!
 //! ### Parsing a WHOIS server response
-//! 
+//!
 //! WHOIS servers often respond to queries by returning multiple related objects.
 //! An example ARIN query for `AS32934` will return with the requested `ASNumber` object first, followed by its associated `OrgName`:
-//! 
+//!
 //! ```sh
 //! $ whois -h whois.arin.net AS32934
 //! ASNumber:       32934
@@ -70,8 +56,8 @@
 //! Updated:        2012-02-24
 //! Comment:        Please send abuse reports to abuse@facebook.com
 //! Ref:            https://rdap.arin.net/registry/autnum/32934
-//! 
-//! 
+//!
+//!
 //! OrgName:        Facebook, Inc.
 //! OrgId:          THEFA-3
 //! Address:        1601 Willow Rd.
@@ -82,16 +68,16 @@
 //! RegDate:        2004-08-11
 //! Updated:        2012-04-17
 //! Ref:            https://rdap.arin.net/registry/entity/THEFA-3
-//! 
-//! 
+//!
+//!
 //! ```
-//! 
-//! To extract each individual object, the [`parse_whois_response`] function can be used to parse the response into a `Vec` containing all individual [Object]s within the response. Examples can be found in the function documentation.
-//! 
+//!
+//! To extract each individual object, the [`parse_whois_response`] function can be used to parse the response into a [`Vec`] containing all individual [`Object`]s within the response. Examples can be found in the function documentation.
+//!
 //! ## Optional Features
-//! 
+//!
 //! The following cargo features can be used to enable additional functionality.
-//! 
+//!
 //! - **simd** _(enabled by default)_: Enables the [Winnow](https://github.com/winnow-rs/winnow) simd feature which improves string search performance using simd.
 //! - **serde**: Enables [Object] serialization using [Serde](https://github.com/serde-rs/serde).
 //! - **json**: Provides JSON serialization of an [Object] using [Serde JSON](https://github.com/serde-rs/json).
